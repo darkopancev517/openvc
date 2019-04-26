7e5b875b2f3efda0ba32cc339a915c2b2f2b6e68
darkopancev <darkopancev.dev@gmail.com>
2019-04-26 16:44:50 +0800

    change uart0 t0 uart1

--- cpu/vc7300x/stdio_uart.c
+++ cpu/vc7300x/stdio_uart.c
@@ -3,22 +3,22 @@
 
 void stdio_init(void)
 {
-    vcmisc_config_apb_periph(APBPERIPH_UART0, true);
-    vcuart_config_reset(UART0);
+    vcmisc_config_apb_periph(APBPERIPH_UART1, true);
+    vcuart_config_reset(UART1);
 
-    vcsfio_enable_uart(UART0);
+    vcsfio_enable_uart(UART1);
 
-    vcuart_config_mode(UART0, UARTMODE_TX);
-    vcuart_config_parity(UART0, UARTPARITY_NONE);
-    vcuart_config_stoplen(UART0, UARTSTOPLEN_1BITS);
-    vcuart_config_wordlen(UART0, UARTWORDLEN_8BITS);
-    vcuart_config_firstbit(UART0, UARTFIRSTBIT_LSB);
-    vcuart_config_baudrate(UART0, 115200);
-    vcuart_config_fifo_softrst(UART0, false);
-    vcuart_config_fifo_ovmode(UART0, false);
+    vcuart_config_mode(UART1, UARTMODE_TX);
+    vcuart_config_parity(UART1, UARTPARITY_NONE);
+    vcuart_config_stoplen(UART1, UARTSTOPLEN_1BITS);
+    vcuart_config_wordlen(UART1, UARTWORDLEN_8BITS);
+    vcuart_config_firstbit(UART1, UARTFIRSTBIT_LSB);
+    vcuart_config_baudrate(UART1, 115200);
+    vcuart_config_fifo_softrst(UART1, false);
+    vcuart_config_fifo_ovmode(UART1, false);
 
-    vcuart_config_fifo_txlevel(UART0, UARTFIFO_TXLVL_7);
-    vcuart_config_fifo_rxlevel(UART0, UARTFIFO_RXLVL_0);
+    vcuart_config_fifo_txlevel(UART1, UARTFIFO_TXLVL_7);
+    vcuart_config_fifo_rxlevel(UART1, UARTFIFO_RXLVL_0);
 }
 
 ssize_t stdio_read(void *buffer, size_t count)
@@ -31,9 +31,9 @@ ssize_t stdio_read(void *buffer, size_t count)
 static size_t uart_write(const uint8_t *data, size_t len)
 {
     for (size_t i= 0; i < len; i++) {
-        vcuart_send_byte(UART0, data[i]);
-        while (!vcuart_get_status(UART0, UARTSTS_TXDONE));
-        vcuart_clear_status(UART0, UARTSTS_TXDONE);
+        vcuart_send_byte(UART1, data[i]);
+        while (!vcuart_get_status(UART1, UARTSTS_TXDONE));
+        vcuart_clear_status(UART1, UARTSTS_TXDONE);
     }
 
     return len;
