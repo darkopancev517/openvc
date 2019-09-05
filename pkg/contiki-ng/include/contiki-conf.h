b2c84bf714309e7741f4c1bb6b838119fee4cb2a
darkopancev <darkopancev.dev@gmail.com>
2019-09-06 00:08:45 +0800

    contiki tsch build passed

--- pkg/contiki-ng/include/contiki-conf.h
+++ pkg/contiki-ng/include/contiki-conf.h
@@ -12,8 +12,23 @@
 extern "C" {
 #endif
 
-#define CLOCK_CONF_SECOND 1000
-#define RTIMER_ARCH_SECOND 1000000
+#define CLOCK_CONF_SECOND (1000)
+#define RTIMER_ARCH_SECOND (1000000)
+
+/* TSCH related config ------------------------------------------------------ */
+#define US_TO_RTIMERTICKS(x) ((uint32_t)(x))
+#define RTIMERTICKS_TO_US(x) ((uint32_t)(x))
+#define RTIMERTICKS_TO_US_64(x) ((uint32_t)(x))
+
+#define RADIO_PHY_OVERHEAD (3)
+#define RADIO_BYTE_AIR_TIME (32)
+#define RADIO_DELAY_BEFORE_TX (US_TO_RTIMERTICKS(500))
+#define RADIO_DELAY_BEFORE_RX (US_TO_RTIMERTICKS(250))
+#define RADIO_DELAY_BEFORE_DETECT (0)
+
+/* Frame filtering done in software */
+#define TSCH_CONF_HW_FRAME_FILTERING  (0)
+/* -------------------------------------------------------------------------- */
 
 #define NETSTACK_CONF_RADIO centauri_driver
 #define centauri_driver_max_payload_len CENTAURI_PAYLOAD_SIZE
