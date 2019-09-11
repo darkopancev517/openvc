b512d2d26472589f59dfe7feb7e84ac1692b79f4
darkopancev <darkopancev.dev@gmail.com>
2019-09-11 16:00:57 +0800

    add int-master printf to watch it was called or not

--- pkg/contiki-ng/contrib/int-master.c
+++ pkg/contiki-ng/contrib/int-master.c
@@ -1,4 +1,5 @@
 #include <stdbool.h>
+#include <stdio.h>
 
 #include "irq.h"
 
@@ -7,20 +8,24 @@
 
 void int_master_enable(void)
 {
-    irq_disable();
+    printf("contiki: int_master_enable\n");
+    irq_enable();
 }
 
 int_master_status_t int_master_read_and_disable(void)
 {
+    printf("contiki: int_master_read_and_disable\n");
     return (int_master_status_t)irq_disable();
 }
 
 void int_master_status_set(int_master_status_t status)
 {
+    printf("contiki: int_master_status_set\n");
     irq_restore((uint32_t)status);
 }
 
 bool int_master_is_enable(void)
 {
+    printf("contiki: int_master_is_enable\n");
     return irq_is_enable() ? true : false;
 }
